5ba839a28ea3f7bc6d5d524589af405ba09a94be
thelitlej <jonasgustafson@hotmail.se>
2018-11-23 10:14:19 +0100

    fixed lower case flagnames

--- src/components/Maps/Flag/Flag.js
+++ src/components/Maps/Flag/Flag.js
@@ -3,11 +3,12 @@ import React from 'react';
 import './Flag.scss';
 
 export default function Flag(props) {
+    var countryCode = props.code.toString().toLowerCase();
 return(
     <React.Fragment>
     <div className="country-display-container">
         <div className="flag-container">
-            <img id='myImage' src={"/flags/"+props.code+".png"} alt={"flag of " + props.name} draggable='false'/>
+            <img id='myImage' src={"/flags/"+countryCode+".png"} alt={"flag of " + props.name} draggable='false'/>
         </div>
         <p className="country-name">{props.name}</p>
     </div>

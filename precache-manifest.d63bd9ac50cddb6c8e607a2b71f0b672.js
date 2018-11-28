d81c76327d79560c3b979fd54d9b8c220e30b47b
thelitlej <jonasgustafson@hotmail.se>
2018-11-28 13:12:20 +0100

    Updates

---
+++ precache-manifest.d63bd9ac50cddb6c8e607a2b71f0b672.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9e03ba334fb62b2bdbe0",
+    "url": "/static/js/main.9e03ba33.chunk.js"
+  },
+  {
+    "revision": "f8f5824ff897738017ce",
+    "url": "/static/js/1.f8f5824f.chunk.js"
+  },
+  {
+    "revision": "9e03ba334fb62b2bdbe0",
+    "url": "/static/css/main.5979805d.chunk.css"
+  },
+  {
+    "revision": "84520d6a059d914066395198e5d9b70d",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/brawl-gaming-webbsite/precache-manifest.dee16af8e48c815387ff5ca043145d9c.js"
+  "/precache-manifest.d63bd9ac50cddb6c8e607a2b71f0b672.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/brawl-gaming-webbsite/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

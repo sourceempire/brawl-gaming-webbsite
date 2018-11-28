1be9d235e70b49e22afec3786bd3667c98e8b40b
thelitlej <jonasgustafson@hotmail.se>
2018-11-28 10:01:20 +0100

    Updates

---
+++ precache-manifest.f320ce34127e6a3b39c149bbb7a85b39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a2b94416182206fbcbf",
+    "url": "/brawlgaming/static/js/runtime~main.6a2b9441.js"
+  },
+  {
+    "revision": "7be6de1f32e04656cd0f",
+    "url": "/brawlgaming/static/js/main.7be6de1f.chunk.js"
+  },
+  {
+    "revision": "71ad8ac0bf0d54025b6b",
+    "url": "/brawlgaming/static/js/1.71ad8ac0.chunk.js"
+  },
+  {
+    "revision": "7be6de1f32e04656cd0f",
+    "url": "/brawlgaming/static/css/main.9b208f9f.chunk.css"
+  },
+  {
+    "revision": "495a692f4ce32d08b57aa96b0f7a9fd8",
+    "url": "/brawlgaming/index.html"
+  }
+];
